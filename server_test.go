8c44075ee733865c2b60bd03e1b604df665a0662
takaya-47 <local.s.s.47@gmail.com>
2026-03-06 05:42:11 +0000

    StoreWinsテストを追加し、POSTリクエストに対するステータスコードの検証を実装

--- server_test.go
+++ server_test.go
@@ -53,6 +53,22 @@ func TestGetPlayers(t *testing.T) {
 	})
 }
 
+func TestStoreWins(t *testing.T) {
+	store := StubPlayerStore{
+		map[string]int{},
+	}
+	server := &PlayerServer{store: &store}
+
+	t.Run("it returns accepted on POST", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodPost, "/players/Pepper", nil)
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assertStatus(t, response.Code, http.StatusAccepted)
+	})
+}
+
 func newGetScoreRequest(name string) *http.Request {
 	request, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/players/%s", name), nil)
 	return request
