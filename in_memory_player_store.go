bb41486b406ec05cf38e2cf46e724629fed10cf9
takaya-47 <local.s.s.47@gmail.com>
2026-03-06 07:00:16 +0000

    InMemoryPlayerStoreにロック機構を追加し、スレッドセーフな勝利記録を実現

--- in_memory_player_store.go
+++ in_memory_player_store.go
@@ -1,14 +1,23 @@
 package main
 
+import "sync"
+
+// PlayerStoreを実装するインメモリのストア
 type InMemoryPlayerStore struct{
 	store map[string]int
+	lock  sync.Mutex
 }
 
 func NewInMemoryPlayerStore() *InMemoryPlayerStore {
-	return &InMemoryPlayerStore{map[string]int{}}
+	return &InMemoryPlayerStore{
+		store: map[string]int{},
+	}
 }
 
 func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	i.lock.Lock()
+	defer i.lock.Unlock()
+
 	if score, ok := i.store[name]; ok {
 		return score
 	}
@@ -16,5 +25,8 @@ func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
 }
 
 func (i *InMemoryPlayerStore) RecordWin(name string) {
+	i.lock.Lock()
+	defer i.lock.Unlock()
+
 	i.store[name]++
 }
