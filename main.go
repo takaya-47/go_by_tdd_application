7b0a8454e8f9627494f058bc3d785aea636927a7
takaya-47 <local.s.s.47@gmail.com>
2026-03-06 05:21:35 +0000

    main関数内のコメントを整理し、InMemoryPlayerStore構造体の定義を整形

--- main.go
+++ main.go
@@ -7,10 +7,11 @@ import (
 
 func main() {
 	server := &PlayerServer{&InMemoryPlayerStore{}}
+	// Webサーバーを起動
 	log.Fatal(http.ListenAndServe(":5000", server))
 }
 
-type InMemoryPlayerStore struct {}
+type InMemoryPlayerStore struct{}
 
 func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
 	return 123
