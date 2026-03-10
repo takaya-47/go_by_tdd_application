477d5c206db52d3c1ae29b5d1b37c97e10b0c38d
takaya-47 <local.s.s.47@gmail.com>
2026-03-10 01:12:43 +0000

    TestRecordingWinsAndRetrievingThem内のスコア取得処理をサブテストに移動

--- server_integration_test.go
+++ server_integration_test.go
@@ -14,9 +14,11 @@ func TestRecordingWinsAndRetrievingThem(t *testing.T) {
 		server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
 	}
 
-	response := httptest.NewRecorder()
-	server.ServeHTTP(response, newGetScoreRequest(player))
+	t.Run("get score", func(t *testing.T) {
+		response := httptest.NewRecorder()
+		server.ServeHTTP(response, newGetScoreRequest(player))
 
-	assertStatus(t, response.Code, http.StatusOK)
-	assertResponseBody(t, response.Body.String(), "3")
+		assertStatus(t, response.Code, http.StatusOK)
+		assertResponseBody(t, response.Body.String(), "3")
+	})
 }
