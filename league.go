d1f893b0656a337b1f238aad80b7ee6bc7162647
takaya-47 <local.s.s.47@gmail.com>
2026-03-11 00:23:51 +0000

    FileSystemPlayerStoreとNewLeagueメソッドを実装し、リーグデータの取得機能を追加

---
+++ league.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+)
+
+func NewLeague(r io.Reader) ([]Player, error) {
+	var league []Player
+	err := json.NewDecoder(r).Decode(&league)
+	if err != nil {
+		err = fmt.Errorf("problem parsing league, %v", err)
+	}
+	return league, err
+}
