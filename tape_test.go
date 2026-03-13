79dc348212c479d1e6278202bd29036dc2117470
takaya-47 <local.s.s.47@gmail.com>
2026-03-13 00:47:32 +0000

    tape_writeのテストを追加

---
+++ tape_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"io"
+	"testing"
+)
+
+func TestTapeWrite(t *testing.T) {
+	file, clean := createTempFile(t, "12345")
+	defer clean()
+
+	tape := &tape{file}
+	tape.Write([]byte("abc")) // "abc"を[]byteにキャスト
+
+	file.Seek(0, io.SeekStart)
+	newFileContents, _ := io.ReadAll(file)
+	got := string(newFileContents) // []byteをstringにキャスト
+	want := "abc"
+
+	if got != want {
+		t.Errorf("got %v want %v", got, want)
+	}
+
+}
