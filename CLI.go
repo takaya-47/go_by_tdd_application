5c324cf3d4c37e09adfa6cad727599d5e4c22e44
takaya-47 <local.s.s.47@gmail.com>
2026-03-17 00:45:29 +0000

    プレイヤー名を「Cleo」から「Chris」に変更

--- CLI.go
+++ CLI.go
@@ -1,9 +1,12 @@
 package poker
 
+import "io"
+
 type CLI struct {
 	store PlayerStore
+	in    io.Reader
 }
 
 func (c *CLI) PlayPoker() {
-	c.store.RecordWin("Cleo") // 固定のプレイヤー名。あとで標準入力から受け取った名前にする。
+	c.store.RecordWin("Chris") // 固定のプレイヤー名。あとで標準入力から受け取った名前にする。
 }
